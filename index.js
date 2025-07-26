97b319a7bf5b12715df8ce1a098a493d2a0ffbdb
Ritika Mehta <ritikamehta@Ritikas-MacBook-Pro.local>
2025-07-26 13:09:58 +0530

    random-password-generator

---
+++ index.js
@@ -0,0 +1,49 @@
+function generatePassword(
+  length,
+  includeLowercase,
+  includeUppercase,
+  includeNumbers,
+  includeSymbols
+) {
+  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numberChars = "0123456789";
+  const symbolChars = "!@#$%^&*~_";
+
+  let allowedChars = "";
+  let password = "";
+
+  allowedChars += includeLowercase ? lowercaseChars : "";
+  allowedChars += includeUppercase ? uppercaseChars : "";
+  allowedChars += includeNumbers ? numberChars : "";
+  allowedChars += includeSymbols ? symbolChars : "";
+
+  if (length <= 0) {
+    return `The password lenght must be at least 1`;
+  }
+  if (allowedChars.length === 0) {
+    return `Aleast one set of characters needs to be selected`;
+  }
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * allowedChars.length);
+    password += allowedChars[randomIndex];
+  }
+  return password;
+
+  console.log(allowedChars);
+  return "";
+}
+
+const passwordLength = 10;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+const password = generatePassword(
+  passwordLength,
+  includeLowercase,
+  includeUppercase,
+  includeNumbers,
+  includeSymbols
+);
+console.log(`Generated password: ${password}`);
